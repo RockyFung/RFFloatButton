ff88431bbeb33b15cb8271bd8aca12fe08068b78
RockyFung <fengjian0213@126.com>
2018-05-17 14:20:42 +0800

    Initial Commit

---
+++ RFFloatButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RFFloatButton
+//
+//  Created by rocky on 2018/5/17.
+//  Copyright © 2018年 rocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
